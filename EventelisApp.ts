089e30565106495dfe0881f766dc2099113e4c25
Felipe Plets <felipe.plets@menvia.com>
2018-12-06 17:49:57 -0200

    Add settings and i18n support

--- EventelisApp.ts
+++ EventelisApp.ts
@@ -1,11 +1,23 @@
 import {
-    ILogger,
+    IConfigurationExtend, IEnvironmentRead, ILogger,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
 
 export class EventelisApp extends App {
     constructor(info: IAppInfo, logger: ILogger) {
         super(info, logger);
     }
+    public async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        await configuration.settings.provideSetting({
+            id: 'key',
+            type: SettingType.STRING,
+            packageValue: '',
+            required: true,
+            public: false,
+            i18nLabel: 'authorization',
+        });
+    }
+
 }
